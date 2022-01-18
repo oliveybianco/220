0c76f06c12505bbb0c3877b4e48b5bde459f61e5
oliveybianco <biancooy@g.cofc.edu>
2022-01-18 15:31:51 -0500

    Completed Lab 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,6 +1,6 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Olivia Bianco>
+<hw1>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
@@ -12,23 +12,34 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area = ", area)
 
 def calc_volume():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume = ", volume)
 
 def shooting_percentage():
-    pass
-
+    total_shots = eval(input("Enter total shots: "))
+    shots_made = eval(input("Enter shots made: "))
+    percentage = shots_made / total_shots
+    print("Shooting Percentage = ", percentage)
 
 def coffee():
-    pass
-
+    c_pounds = eval(input("How many pounds of coffee would you like?" ))
+    coffee_cost = c_pounds * 10.5
+    shipping_cost = coffee_cost * .86
+    total_pounds_cost = shipping_cost + 1.50
+    print("Your total is: ", total_pounds_cost)
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you travel?"))
+    conversion = kilometers / 1.61
 
 
 if __name__ == '__main__':
