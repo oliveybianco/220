58cc9786aec1cbc5f135c0735622fb26e45eb8f3
oliveybianco <biancooy@g.cofc.edu>
2022-01-18 15:37:42 -0500

    Completed Lab 1

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -7,33 +7,23 @@ Problem: <Code to compute the monthly interest charge on a credit card account.>
 Certification of Authenticity:
 <I certify that this assignment is entirely my own work.>
 """
+
+
 def monthly_charge():
-   annual_interest = eval(input("What is the annual interest rate?"))
-   annual_rate = annual_interest / 100
-   num_days = eval(input("What is the number of days in the billing cycle?"))
-   prev_bal = eval(input("What is the previous net balance?"))
-   step_one = num_days * prev_bal
-   pay_amount = eval(input("What was the payment amount?"))
-   pay_date = eval(input("When was this payment made?"))
-   new_paydate = num_days - pay_date
-   step_two = pay_amount * new_paydate
-   step_three = step_one - step_two
-   avg_daily_bal = step_three / num_days
-   monthly_rate = annual_rate / 12
-   monthly_charge = avg_daily_bal * monthly_rate
-   return monthly_charge
+    annual_interest = eval(input("What is the annual interest rate?"))
+    annual_rate = annual_interest / 100
+    num_days = eval(input("What is the number of days in the billing cycle?"))
+    prev_bal = eval(input("What is the previous net balance?"))
+    step_one = num_days * prev_bal
+    pay_amount = eval(input("What was the payment amount?"))
+    pay_date = eval(input("When was this payment made?"))
+    new_pay_date = num_days - pay_date
+    step_two = pay_amount * new_pay_date
+    step_three = step_one - step_two
+    avg_daily_bal = step_three / num_days
+    monthly_rate = annual_rate / 12
+    monthly_charge = avg_daily_bal * monthly_rate
+    return monthly_charge
+
 
 print("The monthly interest charge is:", monthly_charge)
-
-
-
-
-
-
-
-
-
-
-
-
-
