27f828002670e7911d0ec029a374b12eb7bd5337
oliveybianco <biancooy@g.cofc.edu>
2022-02-03 12:27:06 -0500

    Completed HW 3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,53 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Olivia Bianco>
+<hw3>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Variety of for loops using accumulators.>
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+<I certify that this assignment is entirely my own work.>
 """
 
 
 def average():
-    pass
+    avg_amt = eval(input("How many grades will you enter? "))
+    accumulator = 0
+    for _ in range(avg_amt):
+        accumulator += eval(input("Enter grade: "))
+        grade_avg = accumulator / avg_amt
+    print("Average is: ", grade_avg)
 
 
 def tip_jar():
-    pass
+    total = 0
+    for _ in range(5):
+        total += eval(input("How much would you like to donate? "))
+    print("total tips: ", total)
 
 
 def newton():
-    pass
+    num = eval(input("What number do you want to square root? "))
+    refine = eval(input("How many times should we improve the approximation? "))
+    approx = num
+    for _ in range(refine):
+        approx = (approx + (num/approx)) / 2
+    print("The square root is approximately: ", approx)
 
 
 def sequence():
-    pass
+    nums = eval(input("Enter the number of terms: "))
+    for i in range(0, nums):
+        print(1 + (i // 2 * 2), end=' ')
 
 
 def pi():
-    pass
+    nums = eval(input("Enter the number of terms in the series: "))
+    answer = 0
+    for i in range(0, nums):
+        numerator = i // 2 * 2
+        denominator = 1 + (i // 2 * 2)
+
+    print("pi is: ", answer)
 
 
 if __name__ == '__main__':
