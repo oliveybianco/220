782a0746805fe5aff327189a0e24f41a14772e71
oliveybianco <biancooy@g.cofc.edu>
2022-02-10 20:05:43 -0500

    Completed lab 4

--- assignments/hw4/hw4.py
+++ assignments/hw4/hw4.py
@@ -1,63 +1,119 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Olivia Bianco>
+<Hw4>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Intro to using python graphics.>
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+<I certify that this assignment is entirely my own work.>
 """
 
-from graphics import *
+import math
+from graphics import GraphWin, Point, Rectangle, Text, Circle
 
 
 def squares():
-    # Creates a graphical window
     width = 400
     height = 400
     win = GraphWin("Clicks", width, height)
-
-    # number of times user can move circle
+    win.setBackground("white")
     num_clicks = 5
 
-    # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to draw a square")
     instructions.draw(win)
 
-    # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    shape = Rectangle(Point(200, 150), Point(150, 200))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
 
-    # allows the user to click multiple times to move the circle
-    for i in range(num_clicks):
+    for _ in range(num_clicks):
         click = win.getMouse()
-        center = shape.getCenter()  # center of circle
 
-        # move amount is distance from center of circle to the
-        # point where the user clicked
-        change_x = click.getX() - center.getX()
-        change_y = click.getY() - center.getY()
-        shape.move(change_x, change_y)
-
+        change_x = click.getX()
+        change_y = click.getY()
+        new_shape1 = Point(change_x - 25, change_y - 25)
+        new_shape2 = Point(change_x + 25, change_y + 25)
+        new_shape = Rectangle(new_shape1, new_shape2)
+        new_shape.setOutline("red")
+        new_shape.setFill("red")
+        new_shape.draw(win)
+    close = Text(Point(200, 200), "Click again to close")
+    close.draw(win)
     win.getMouse()
     win.close()
 
 
 def rectangle():
-    pass
+    win = GraphWin("Rectangle", 400, 400)
+    win.setBackground("white")
+    p_1 = win.getMouse()
+    p_2 = win.getMouse()
+    p1_x = p_1.getX()
+    p1_y = p_1.getY()
+    p2_x = p_2.getX()
+    p2_y = p_2.getY()
+    rec = Rectangle(Point(p1_x, p1_y), Point(p2_x, p2_y))
+    rec.setFill("green")
+    rec.setOutline("green")
+    rec.draw(win)
+
+    perimeter = 2 * (p2_x + p2_y)
+    per_info = Text(Point(175, 350), "Perimeter: ")
+    per_info.draw(win)
+    per_value = Text(Point(220, 350), perimeter)
+    per_value.draw(win)
+    rec_area = (p1_x - p2_x) * (p1_y - p2_y)
+    a_info = Text(Point(175, 375), "Area: ")
+    a_info.draw(win)
+    a_value = Text(Point(210, 375), rec_area)
+    a_value.draw(win)
+
+    instructions = Text(Point(200, 200), "Click again to close")
+    instructions.draw(win)
+    win.getMouse()
+    win.close()
 
 
 def circle():
-    pass
+    win = GraphWin("Circle", 400, 400)
+    win.setBackground("white")
+    middle = Point(200, 200)
+    close = Text(middle, "Click again to close")
+    close.draw(win)
+    center = win.getMouse()
+    center.draw(win)
+    c_point = win.getMouse()
+    c_point.draw(win)
+    x_val = (center.getX() - c_point.getX()) ** 2
+    y_val = (center.getY() - c_point.getY()) ** 2
+    distance = math.sqrt(x_val + y_val)
+    circ = Circle(center, distance)
+    circ.draw(win)
+    circ.setOutline("black")
+    circ.setFill("cyan")
+    p_info = Point(130, 375)
+    radius = Text(p_info, "Radius: ")
+    radius.draw(win)
+    num = Text(Point(210, 375), distance)
+    num.draw(win)
+    win.getMouse()
+    win.close()
 
 
 def pi2():
-    pass
+    terms = eval(input("How many terms are there? "))
+    acc = 0
+    numerator = 4
+    denominator = 1
+    change = 1
+    for _ in range(terms):
+        acc = acc + change * (numerator / denominator)
+        denominator = denominator + 2
+        change = change * -1
+    print("pi approximation: ", acc)
+    print("accuracy: ", abs(math.pi-acc))
 
 
 if __name__ == '__main__':
